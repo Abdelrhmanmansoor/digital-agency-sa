f48890e2504905999430cf7aea4f3c8a6d4289c7
Abdelrhmanmansoor <mansoorsoliman77@gmail.com>
2026-02-27 05:35:06 +0200

    fix: remove clsx/tailwind-merge deps from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: string[]) {
+  return inputs.filter(Boolean).join(" ");
 }
 
 export const WHATSAPP_NUMBER = "+201007835547";
